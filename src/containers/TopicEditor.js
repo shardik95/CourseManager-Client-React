cffc55c74358c4b30d37df1a7efe1dab56758bbb
shardik95 <shardik95@ccs.neu.edu>
2018-05-28 00:51:29 -0400

    linked topic to widgets

--- src/containers/TopicEditor.js
+++ src/containers/TopicEditor.js
@@ -50,24 +50,60 @@ class TopicEditor extends React.Component{
         this.setState({lessonId:lessonId})
     }
 
+
+
     render(){
+
         return(
             <Provider store={store}>
+
                 <WidgetContainer courseId={this.state.courseId}
                 moduleId={this.state.moduleId}
                 lessonId={this.state.moduleId}
                 topicId={this.state.topicId}
-                widgets={[
-                    {title:'Widget 1'},
-                    {title:'Widget 2'}
-                ]}/>
+                />
             </Provider>
         )
     }
 }
 
-const WidgetReducer = (state={widgets:[]},action)=>{
+const WidgetReducer = (state={
+    widgets:[],topicId:""}, action)=>{
     switch (action.type){
+
+        case 'FIND_WIDGETS_TOPIC':return{
+            widgets:action.widgets,
+            topicId:action.topicId
+        }
+
+
+        case 'ADD':return {
+            widgets:[...state.widgets,
+                {
+                    text:'New Widget',
+                    topicId:action.topicId,
+                    widgetId:state.widgets.length+1
+                }
+            ],
+            topicId:action.topicId
+        }
+
+        case 'DELETE': return {
+            widgets: state.widgets.filter(widget=>(
+                widget.id !== action.widgetId
+            )),
+            topicId:action.topicId
+        }
+
+        case 'SAVE': fetch('http://localhost:8080/api/topic/TID/widget/save'.replace('TID',action.topicId),{
+            method:'POST',
+            body:JSON.stringify(state.widgets),
+            headers:{
+                'content-type': 'application/json'
+            }
+        })
+            return state;
+
         default: return state;
     }
 }
--- src/containers/WidgetList.js
+++ src/containers/WidgetList.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import {connect} from 'react-redux';
 
-export const Widget = ({widget}) =>(
-    <li>Widget {widget.title}
-        <button>Delete Widget</button>
+export const Widget = ({widget,topicId,dispatch}) =>(
+    <li>{widget.text}
+        <button onClick={()=>{
+            dispatch({type:'DELETE',widgetId:widget.id,topicId:topicId})
+        }}>Delete Widget</button>
     </li>
 )
+
+const WidgetCont= connect()(Widget);
 
 
 class WidgetList extends React.Component{
@@ -14,22 +18,64 @@ class WidgetList extends React.Component{
         super(props)
     }
 
+    componentDidMount(){
+    }
+
+    componentWillReceiveProps(newProps){
+        if(newProps.topicId!==this.props.topicId){
+            this.props.findWidgetsForTopic(newProps.topicId);
+        }
+    }
+
     render(){
         return(
             <div>
+                <br/>
+                <h2>Widget List {this.props.widgets.length}</h2>
+                <button onClick={()=>{this.props.save(this.props.topicId)}} >Save</button>
                 <ul>
                     {this.props.widgets.map(
                         widget => (
-                            <Widget widget={widget}/>
+                            <div>
+                            <WidgetCont key={widget.id} widget={widget} topicId={this.props.topicId}/>
+                            </div>
                         )
                     )}
                 </ul>
-                <butto>Add Widget</butto>
+                <button onClick={()=>{this.props.add(this.props.topicId)}}>Add Widget</button>
             </div>
         )
     }
 
 }
 
+const findWidgetsForTopic = (topicId,dispatch) => (
+    fetch("http://localhost:8080/api/topic/TID/widget".replace("TID",topicId))
+        .then(response => (response.json()))
+        .then(widgets => (dispatch(
+            {type:'FIND_WIDGETS_TOPIC',
+                widgets:widgets,topicId:topicId}
+        )))
+)
 
-export const WidgetContainer = connect()(WidgetList);
+const save = (topicId,dispatch) =>(
+    dispatch({type:"SAVE",topicId:topicId})
+)
+
+const add = (topicId,dispatch) => (
+    dispatch({type:'ADD',topicId:topicId})
+)
+
+const dispatchToPropsMapper =(dispatch)=>({
+    findWidgetsForTopic: (topicId) => findWidgetsForTopic(topicId,dispatch),
+    save: (topicId)=> save(topicId,dispatch),
+    add: (topicId) => add(topicId,dispatch)
+})
+
+const stateToPropsMapper = state => (
+    {
+        widgets:state.widgets,
+    }
+)
+
+export const WidgetContainer = connect(stateToPropsMapper,dispatchToPropsMapper)(WidgetList);
